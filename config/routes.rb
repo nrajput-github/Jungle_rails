25a257e82ba414c28772fe6dea223680add653ae
Navya Rajput <navyasrajput@gmail.com>
2021-11-11 23:59:31 -0500

    Added Feature: User Authentication

--- config/routes.rb
+++ config/routes.rb
@@ -12,12 +12,19 @@ Rails.application.routes.draw do
     post   :remove_item
   end
 
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+ get '/signup' => 'users#new'
+  post '/users' => 'users#create'
   resources :orders, only: [:create, :show]
 
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
     resources :categories, except: [:edit, :update, :show, :destroy]
+    
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
